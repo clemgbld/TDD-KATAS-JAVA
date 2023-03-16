d9673de435b286b1107d5cf5eb0905551c76c41a
cgombauld <clement.gombauld@cpexterne.org>
2023-03-16 14:09:56 +0100

    refacto

--- src/countingDuplicates/CountingDuplicates.java
+++ src/countingDuplicates/CountingDuplicates.java
@@ -5,13 +5,12 @@ import java.util.stream.Collectors;
 
 public class CountingDuplicates {
     public static int duplicateCount(String s) {
-        return s.chars()
+        return (int) s.chars()
                 .map(Character::toLowerCase)
                 .mapToObj(c -> (char) c)
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
                 .entrySet()
                 .stream().filter(e -> e.getValue() > 1)
-                .toList()
-                .size();
+                .count();
     }
 }
