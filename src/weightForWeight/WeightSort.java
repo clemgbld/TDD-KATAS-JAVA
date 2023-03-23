02b45bf20d32d4a2bc41bc002cfd394300c2dadc
cgombauld <clement.gombauld@cpexterne.org>
2023-03-23 19:02:54 +0100

    implemented weight for weight

--- src/weightForWeight/WeightSort.java
+++ src/weightForWeight/WeightSort.java
@@ -1,4 +1,23 @@
 package weightForWeight;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class WeightSort {
+    public static String orderWeight(String numsString) {
+      return Arrays.stream(numsString.trim().split("\\s+"))
+              .sorted((s1,s2)->shouldSwitch(s1,s2) ? 1 : -1 )
+              .collect(Collectors.joining(" "));
+    }
+
+    private static boolean shouldSwitch (String strNum1, String strNum2){
+        int sum1 = sumAllDigits(strNum1);
+        int sum2 = sumAllDigits(strNum2);
+        if(sum1 == sum2) return strNum1.compareTo(strNum2) > 0;
+        return sum1 > sum2;
+    }
+
+    private static int sumAllDigits(String strNum){
+      return Arrays.stream(strNum.split("")).mapToInt(Integer::valueOf).sum();
+    }
 }
--- src/weightForWeight/WeightSortTest.java
+++ src/weightForWeight/WeightSortTest.java
@@ -1,7 +1,19 @@
 package weightForWeight;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class WeightSortTest {
+    @Test
+    public void shouldOrderNumbersStringByDigitSum(){
+        assertEquals("1 2",WeightSort.orderWeight("1 2"));
+        assertEquals("1 2",WeightSort.orderWeight("2 1"));
+        assertEquals("1 2",WeightSort.orderWeight(" 1 2"));
+        assertEquals("1 2",WeightSort.orderWeight(" 1   2"));
+        assertEquals("11 3",WeightSort.orderWeight("11 3"));
+        assertEquals("11 2",WeightSort.orderWeight("2 11"));
+        assertEquals("1 2 3",WeightSort.orderWeight("3 2 1"));
+    }
 
 }
