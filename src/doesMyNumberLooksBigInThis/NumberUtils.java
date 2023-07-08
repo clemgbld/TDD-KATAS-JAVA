a606a46764ff691082860df2e0f3e4854064623f
cgombauld <clement.gombauld@cpexterne.org>
2023-07-08 11:14:14 +0200

    refacto

--- src/doesMyNumberLooksBigInThis/NumberUtils.java
+++ src/doesMyNumberLooksBigInThis/NumberUtils.java
@@ -4,10 +4,9 @@ import java.util.Arrays;
 
 public class NumberUtils {
     public static boolean isNarcissistic(int num) {
-        var strNums = Integer.toString(num).split("");
-
-        return Arrays.stream(strNums).mapToInt(Integer::parseInt)
-                .mapToDouble(n-> Math.pow(n,strNums.length))
+        return Arrays.stream(Integer.toString(num).split(""))
+                .mapToInt(Integer::parseInt)
+                .mapToDouble(n-> Math.pow(n,Integer.toString(num).length()))
                 .sum() == num;
     }
 }
