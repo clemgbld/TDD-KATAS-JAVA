c8a235485e7f353a36bb68d1c27879f81b4954e0
cgombauld <clement.gombauld@cpexterne.org>
2023-07-18 08:20:46 +0200

    save

--- src/rotateArray/Rotator.java
+++ src/rotateArray/Rotator.java
@@ -1,2 +1,14 @@
-package rotateArray;public class Rotator {
+package rotateArray;
+
+
+import java.util.Arrays;
+import java.util.Collections;
+
+
+public class Rotator {
+    public Object[] rotate(Object[] data, int n) {
+        Object[] dataToRotate = Arrays.copyOf(data,data.length);
+        Collections.rotate(Arrays.asList(dataToRotate),n);
+        return dataToRotate;
+    }
 }
--- src/rotateArray/RotatorTest.java
+++ src/rotateArray/RotatorTest.java
@@ -1,4 +1,20 @@
+package rotateArray;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class RotatorTest {
-  
+    @Test
+    void shouldRotateTheArrayNTimes(){
+        var rotator = new Rotator();
+        assertArrayEquals(new Object[]{1,2,3,4,5},rotator.rotate(new Object[]{1,2,3,4,5},0));
+        assertArrayEquals(new Object[]{1,2,3,4},rotator.rotate(new Object[]{1,2,3,4},0));
+        assertArrayEquals(new Object[]{5,1,2,3,4},rotator.rotate(new Object[]{1,2,3,4,5},1));
+        assertArrayEquals(new Object[]{2,3,4,5,1},rotator.rotate(new Object[]{1,2,3,4,5},-1));
+        assertArrayEquals(new Object[]{2, 3, 4, 5, 1},rotator.rotate(new Object[]{1,2,3,4,5},4));
+        assertArrayEquals(new Object[]{5, 1, 2, 3, 4},rotator.rotate(new Object[]{1,2,3,4,5},-4));
+        assertArrayEquals(new Object[]{3, 4, 5, 1, 2}, rotator.rotate(new Object[]{1,2,3,4,5},12478));
+    }
+
 }
